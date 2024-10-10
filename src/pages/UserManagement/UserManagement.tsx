7d4162738fe52924551765e522e2ba9344a45a49
dev-dumbo98 <dumbo98@catalonix.com>
2024-10-10 16:04:15 +0900

    user modal

--- src/pages/UserManagement/UserManagement.tsx
+++ src/pages/UserManagement/UserManagement.tsx
@@ -1,4 +1,4 @@
-import { Button, DatePicker, Select } from 'antd';
+import { Button, DatePicker, Form, Select, Input } from 'antd';
 import Card from 'components/common/Card';
 import { userSearchOptions } from 'common/constants/UserManagement';
 import { SearchOutlined, UserAddOutlined } from '@ant-design/icons';
@@ -7,6 +7,25 @@ import Modal from 'components/common/Modal';
 
 const UserManagment = () => {
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const formItemLayout = {
+    labelCol: {
+      xs: {
+        span: 24
+      },
+      sm: {
+        span: 8
+      }
+    },
+    wrapperCol: {
+      xs: {
+        span: 24
+      },
+      sm: {
+        span: 16
+      }
+    }
+  };
 
   const handleClickUserAddBtn = () => {
     setIsModalVisible(true);
@@ -39,12 +58,90 @@ const UserManagment = () => {
             회원 추가
           </Button>
         }>
-        <div>
-          <div className="search-content"></div>
-        </div>
+        <div className="search-content"></div>
       </Card>
 
-      <Modal title="회원 추가" isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel} />
+      <Modal title="회원 추가" isModalVisible={isModalVisible} handleOk={handleOk} handleCancel={handleCancel}>
+        <Form style={{ marginTop: '20px' }} {...formItemLayout} className="antd-form">
+          <Form.Item
+            label="아이디"
+            name="id"
+            rules={[
+              {
+                required: true,
+                message: '아이디를 입력해주세요.'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="비밀번호"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '비밀번호를 입력해주세요.'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="비밀번호 확인"
+            name="passwordCheck"
+            rules={[
+              {
+                required: true,
+                message: 'Please input!'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="담당자"
+            name="variant"
+            rules={[
+              {
+                required: true,
+                message: 'Please input!'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="권한설정"
+            name="variant"
+            rules={[
+              {
+                required: true,
+                message: 'Please input!'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="부서"
+            name="variant"
+            rules={[
+              {
+                required: true,
+                message: 'Please input!'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="연락처"
+            name="variant"
+            rules={[
+              {
+                required: true,
+                message: 'Please input!'
+              }
+            ]}>
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
     </div>
   );
 };
