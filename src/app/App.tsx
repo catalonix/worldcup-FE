c083a104758b1570c77d8c375e717dd3d83f2902
dev-dumbo98 <dumbo98@catalonix.com>
2024-10-15 11:00:21 +0900

    chartjs 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import { ConfigProvider, theme } from 'antd';
 import { Routing } from './routing';
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  LineController
+} from 'chart.js';
+
 function App() {
+  ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, LineController);
+
   return (
     <>
       <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
--- src/pages/MonitoringEquipment/SoilDetail/SoilDetail.tsx
+++ src/pages/MonitoringEquipment/SoilDetail/SoilDetail.tsx
@@ -1,21 +1,57 @@
 import React from 'react';
-
+import { Chart } from 'react-chartjs-2';
 import Card from 'components/common/Card';
 import { Button, DatePicker, Select } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { soilSearchOptions } from 'common/constants/soilDetail';
 
 const SoilDetail = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const
+      }
+    }
+  };
+
+  const data = {
+    labels: ['1', '2', '3'], // x축을 나타내는 가상의 레이블
+    datasets: [
+      {
+        label: '토양습도',
+        backgroundColor: 'rgba(0, 0, 255, 0.5)',
+        borderColor: 'blue',
+        data: [3, 5, 11] // y 값
+      },
+      {
+        label: '토양온도',
+        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+        borderColor: 'red',
+        data: [5, 10, 10] // y 값
+      },
+      {
+        label: '토양양분',
+        backgroundColor: 'rgba(255, 165, 0, 0.5)',
+        borderColor: 'orange',
+        data: [15, 23, 20] // y 값
+      }
+    ]
+  };
+
   return (
     <div>
       <Card title="상세검색">
-        <div>
-          <div className="search-content">
-            <Select options={soilSearchOptions} defaultValue={'soilRobot'} style={{ width: '20%' }} />
-            <DatePicker />
-            <DatePicker />
-            <Button icon={<SearchOutlined />}>조회하기</Button>
-          </div>
+        <div className="search-content">
+          <Select options={soilSearchOptions} defaultValue={'soilRobot'} style={{ width: '20%' }} />
+          <DatePicker />
+          <DatePicker />
+          <Button icon={<SearchOutlined />}>조회하기</Button>
+        </div>
+      </Card>
+      <Card title="관측정보">
+        <div className="search-content">
+          <Chart type="line" data={data} options={options} />
         </div>
       </Card>
     </div>
