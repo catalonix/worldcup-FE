3f45ddae4ddb376bbfff45b8e8f4a0547fe2709e
dev-dumbo98 <dumbo98@catalonix.com>
2024-10-15 12:58:59 +0900

    header 반응형

--- src/app/layout/DefaultLayout.tsx
+++ src/app/layout/DefaultLayout.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Outlet } from 'react-router-dom';
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
+  AlignLeftOutlined,
   AppstoreOutlined,
   FileOutlined,
   UserOutlined,
   CopyOutlined,
-  StockOutlined
+  StockOutlined,
+  MoreOutlined
 } from '@ant-design/icons';
 import { Button, Layout, Menu, theme, Tag, Avatar, Dropdown } from 'antd';
 import type { MenuProps } from 'antd';
@@ -22,6 +22,8 @@ const DefaultLayout: React.FC = () => {
   const navigate = useNavigate();
 
   const [collapsed, setCollapsed] = useState<boolean>(false);
+  const [isOpenWeatherInfo, setIsOpenWeatherInfo] = useState<boolean>(false);
+
   const [selectedKeys, setSelectedKeys] = useState<string[]>([]);
 
   const {
@@ -71,16 +73,41 @@ const DefaultLayout: React.FC = () => {
           {!collapsed && <span style={{ fontSize: '22px', color: 'white', fontWeight: '700' }}>서울월드컵경기장</span>}
           <Button
             type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            icon={<AlignLeftOutlined />}
             onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: '22px',
-              width: 64,
-              height: 64,
               color: 'white'
             }}
           />
+          <Button
+            type="text"
+            icon={<MoreOutlined />}
+            onClick={() => setIsOpenWeatherInfo(!isOpenWeatherInfo)}
+            className="more-btn"
+          />
         </div>
+
+        {isOpenWeatherInfo && (
+          <div className="weather-info__mobile">
+            <div style={{ display: 'flex' }}>
+              <div className="weather-tag">
+                미세먼지
+                <Tag color="blue" style={{ fontSize: '10px' }}>
+                  좋음
+                </Tag>
+              </div>
+              <div className="weather-tag">
+                초미세먼지
+                <Tag color="blue" style={{ fontSize: '10px' }}>
+                  좋음
+                </Tag>
+              </div>
+            </div>
+
+            <WeatherInfo />
+          </div>
+        )}
 
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
           <div className="weather-tag">
