9aafb312ea1737cd60524ccec83e48abec8b3c25
dev-dumbo98 <dumbo98@catalonix.com>
2024-10-15 10:38:53 +0900

    routing 수정

--- src/app/routing/app-routing.ts
+++ src/app/routing/app-routing.ts
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 export enum AppPaths {
-  HOME = 'home',
+  HOME = '',
   LOGIN = 'login',
   MONITORING_EQUIPMENT = 'monitoringEquipment',
   NDVI_SUMMARY = 'ndviSummary',
