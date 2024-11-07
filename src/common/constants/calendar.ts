3b8400e8c3417d788fb2641202c72a7e7ec45499
dev-dumbo98 <dumbo98@catalonix.com>
2024-11-07 16:08:25 +0900

    calendar border custom

---
+++ src/common/constants/calendar.ts
@@ -0,0 +1,19 @@
+export const calendarSearchOptions = [
+  { label: '쿨링팬', value: '' },
+  { label: '관수', value: '' },
+  { label: '깎기', value: '' },
+  { label: '시약', value: '' },
+  { label: '시비', value: '' },
+  { label: '갱신', value: '' },
+  { label: '배토', value: '' },
+  { label: '다짐', value: '' },
+  { label: '보수', value: '' },
+  { label: '제조', value: '' },
+  { label: '청소', value: '' },
+  { label: '경기', value: '' },
+  { label: '보식', value: '' },
+  { label: '파종', value: '' },
+  { label: '계절', value: '' },
+  { label: '장비', value: '' },
+  { label: '기타', value: '' }
+];
--- src/components/Calendar/Toolbar/Toolbar.tsx
+++ src/components/Calendar/Toolbar/Toolbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, Checkbox, GetProp } from 'antd';
 
 import { DownloadOutlined, LeftOutlined, RightOutlined, PlusSquareOutlined } from '@ant-design/icons';
+import { calendarSearchOptions } from 'common/constants/\bcalendar';
 
 type CustomToolbarProps = {
   date: Date;
@@ -17,20 +18,38 @@ const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
     alert('download');
   };
 
+  const handleChangeDirection: GetProp<typeof Checkbox.Group, 'onChange'> = checkedValues => {
+    console.log('value', checkedValues);
+  };
+
   return (
     <div className="rbc-toolbar">
-      <span className="rbc-btn-group">
-        <Button icon={<LeftOutlined />} onClick={() => navigate('PREV')}></Button>
-        <Button icon={<RightOutlined />} onClick={() => navigate('NEXT')}></Button>
-        <Button onClick={() => navigate('TODAY')}>오늘</Button>
-      </span>
-      <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
-      <span className="rbc-btn-group">
-        <Button icon={<PlusSquareOutlined />} onClick={handleDownloadCalendar}>
-          추가
-        </Button>
-        <Button icon={<DownloadOutlined />} onClick={handleDownloadCalendar} style={{ background: '#374b64' }}></Button>
-      </span>
+      <div className="d-flex align-center justify-space-between w-100">
+        <span className="rbc-btn-group">
+          <Button icon={<LeftOutlined />} onClick={() => navigate('PREV')}></Button>
+          <Button icon={<RightOutlined />} onClick={() => navigate('NEXT')}></Button>
+          <Button onClick={() => navigate('TODAY')}>오늘</Button>
+        </span>
+        <span className="rbc-toolbar-label">{`${date.getFullYear()}년 ${date.getMonth() + 1}월`}</span>
+        <span className="rbc-btn-group">
+          <Button icon={<PlusSquareOutlined />} onClick={handleDownloadCalendar}>
+            추가
+          </Button>
+          <Button
+            icon={<DownloadOutlined />}
+            onClick={handleDownloadCalendar}
+            style={{ background: '#374b64' }}></Button>
+        </span>
+      </div>
+
+      <div style={{ marginTop: '0.5rem' }}>
+        <Checkbox.Group
+          options={calendarSearchOptions}
+          defaultValue={['EN', 'WS', 'SE', 'WN']}
+          onChange={handleChangeDirection}
+          className="checkbox-group"
+        />
+      </div>
     </div>
   );
 };
