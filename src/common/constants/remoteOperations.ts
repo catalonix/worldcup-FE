00dae3d66ff4cbb5eba19de4215d5789e70ea2b9
dev-dumbo98 <dumbo98@catalonix.com>
2024-11-28 17:41:31 +0900

    calendar

--- src/common/constants/remoteOperations.ts
+++ src/common/constants/remoteOperations.ts
@@ -3,10 +3,10 @@ export const remoteOperationFanOptions = [
   { label: '2', value: '2' },
   { label: '3', value: '3' },
   { label: '4', value: '4' },
-  { label: '5', value: '쿨링팬' },
-  { label: '6', value: '쿨링팬' },
-  { label: '7', value: '쿨링팬' },
-  { label: '8', value: '쿨링팬' },
+  { label: '5', value: '5' },
+  { label: '6', value: '6' },
+  { label: '7', value: '7' },
+  { label: '8', value: '8' },
   { label: '테스트', value: '쿨링팬' }
 ];
 
@@ -15,6 +15,6 @@ export const timeOptions = [
   { label: '3분', value: '3' },
   { label: '5분', value: '5' },
   { label: '10분', value: '10' },
-  { label: '15분', value: '쿨링팬' },
-  { label: '20분', value: '쿨링팬' }
+  { label: '15분', value: '15' },
+  { label: '20분', value: '20' }
 ];
